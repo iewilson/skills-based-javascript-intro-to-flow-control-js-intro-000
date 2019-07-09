3a9f2e625ce5aaf5aa5f51b42b7de3e61e907c61
Isabel Wilson <isabel.elena.wilson@gmail.com>
2019-07-09 14:05:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,13 +6,12 @@ return "You are a teenager!";
 
 
 function teenager(age) {
-  if  {
-    (age > 13 && age<19)
+  if  (age >= 13 && age=<19) {
     return "You are a teenager"
-  }
-  else {
+} else {
     return "You are not a teenager"
   }
+}
 
 function ageChecker(age) {
 
